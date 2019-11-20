bf6fea87441e79e0ec2ff7ce6f925edb1e00fe1c
Carlos Turcios <turcios50@gmail.com>
2019-11-19 19:48:18 -0800

    finished project

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React from 'react';
 
-const NumberButton = () => {
+function NumberButton(props) {
+  //const {number} = props;
   return (
-    <>
+    <button className='numsButton'>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.number}
+    </button>
   );
-};
+}
+export default NumberButton;
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,7 +1,8 @@
-import React from "react";
-
+import React, { useState } from 'react';
+import { numbers } from '../../../data.js';
+import NumberButton from './NumberButton.js';
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
+// example of import from data.js. Note all the ../   This is how we move through folders.
 /* 
 import { numbers } from '../../../data' 
 */
@@ -9,11 +10,19 @@ import { numbers } from '../../../data'
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [nums, setNumber] = useState(numbers);
+  // const [buttonState] = useState(NumberButton);
   return (
-    <div>
+    <>
+      <div className='button_container'>
+        {nums.map(index => (
+          <NumberButton number={index} />
+        ))}
+      </div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-    </div>
+    </>
   );
 };
+export default Numbers;
